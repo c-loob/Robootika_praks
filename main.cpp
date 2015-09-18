05594fc55d0a4e79fe76c1edd2f56066c1c7b257
c-loob <chrstrloob@gmail.com>
2015-09-18 22:53:22 +0300

    veel kommentaare
    delfi

--- main.cpp
+++ main.cpp
@@ -10,12 +10,13 @@ int main() {
 	VideoCapture cap(0);//enter cam # or video location
 	if (!cap.isOpened()) return -1; //check if succeeded
 
-	namedWindow("frame", WINDOW_AUTOSIZE);
+	namedWindow("frame", WINDOW_AUTOSIZE);//captured img
 	namedWindow("control", WINDOW_AUTOSIZE);//trackbaride aken
 	namedWindow("frameThresh", WINDOW_AUTOSIZE);//threshold aken
 
+	//initial values for trackbars
 	int lowH = 0;
-	int highH = 179;
+	int highH = 25;
 	
 	int lowS = 0;
 	int highS = 255;
@@ -23,7 +24,7 @@ int main() {
 	int lowV = 0;
 	int highV = 255;
 
-	//trackbars
+	//trackbar creation
 	cvCreateTrackbar("LowH", "control", &lowH, 179);//hue
 	cvCreateTrackbar("HighH", "control", &highH, 179);
 
@@ -36,13 +37,13 @@ int main() {
 	for (;;) {
 		Mat frame;//frame
 		cap >> frame;
-		if (!cap.read(frame)) break;
+		if (!cap.read(frame)) break;//check for error
 
 		Mat frameHSV;//frame in HSV
 		cvtColor(frame, frameHSV, COLOR_BGR2HSV);//to HSV color space
 
 		Mat frameThresh;//frame post thresh
-		inRange(frameHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), frameThresh);
+		inRange(frameHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), frameThresh);//threshold img in ranges
 
 		imshow("frame", frame);
 		imshow("frameThresh", frameThresh);
