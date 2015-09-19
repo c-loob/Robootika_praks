fd5a368ec0b3d23edf24e5c96410a468e3193ffe
c-loob <chrstrloob@gmail.com>
2015-09-19 12:08:10 +0300

    Parandatud palli tracker_töötab
    yo

--- main.cpp
+++ main.cpp
@@ -29,37 +29,44 @@ int main() {
 	int lowV = 100;
 	int highV = 255;
 
+	int hi = 100, lo = 80;
+
 	//trackbar creation
-	cvCreateTrackbar("LowH", "control", &lowH, 179);//hue
-	cvCreateTrackbar("HighH", "control", &highH, 179);
+	createTrackbar("hi", "control", &hi, 255);
+	createTrackbar("lo", "control", &lo, 255);
 
-	cvCreateTrackbar("LowS", "control", &lowS, 255);//saturation
-	cvCreateTrackbar("HighS", "control", &highS, 255);
+	createTrackbar("LowH", "control", &lowH, 179);//hue
+	createTrackbar("HighH", "control", &highH, 179);
 
-	cvCreateTrackbar("LowV", "control", &lowV, 255);//value
-	cvCreateTrackbar("HighV", "control", &highV, 255);
+	createTrackbar("LowS", "control", &lowS, 255);//saturation
+	createTrackbar("HighS", "control", &highS, 255);
+
+	createTrackbar("LowV", "control", &lowV, 255);//value
+	createTrackbar("HighV", "control", &highV, 255);
+
 	vector<Vec3f> circles;
+	int erosion_size = 6;
+	Mat element = getStructuringElement(MORPH_CROSS, Size(2 * erosion_size + 1, 2 * erosion_size + 1), Point(erosion_size, erosion_size));
 	for (;;) {
 		Mat frame;//frame
 		cap >> frame;
 		if (!cap.read(frame)) break;//check for error
 
+		//HSV threshold:
 		Mat frameHSV,hsv_channels[3];//frame in HSV
 		cvtColor(frame, frameHSV, COLOR_BGR2HSV);//to HSV color space
-		
 		Mat frameThresh;//frame post thresh
 		inRange(frameHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), frameThresh);//threshold img in ranges
+
+		//houghcircles:
 		Mat gray;
-		//medianBlur(frame, frame, 3);
-		GaussianBlur(frameThresh, gray, Size(5, 5), 1.5, 1.5);
+		erode(frameThresh, gray, element);
+		dilate(gray, gray, element);
+		GaussianBlur(gray, gray, Size(5, 5), 2, 2);
 		
-		//split(frameHSV, hsv_channels);//channel 2 ==value(grayscale)
-		
-		
-		//cvtColor(frame, gray, COLOR_BGR2GRAY);
-		if (gray.channels()==1){
-			HoughCircles(gray, circles, HOUGH_GRADIENT, 2.0, 120, 200, 100);
-		}
+		HoughCircles(gray, circles, HOUGH_GRADIENT, 2.0, 20.0, hi>0?hi:1, lo >0?lo:1);
+
+		//draw detected circles:
 		for (size_t i = 0; i < circles.size(); i++) {
 			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 			int radius = cvRound(circles[i][2]);
@@ -67,6 +74,9 @@ int main() {
 			circle(frame, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 			
 		}
+
+
+		//display:
 		imshow("frame", frame);
 		imshow("frameThresh", gray);
 		if (waitKey(30) >= 0) break;//nupuvajutuse peale break
