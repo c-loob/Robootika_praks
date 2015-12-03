a5ed8b4359e08033c1572c92682d9ffb450f83cd
c-loob <chrstrloob@gmail.com>
2015-12-03 19:50:05 +0200

    save
    save

--- main.cpp
+++ main.cpp
@@ -27,8 +27,14 @@ movement(liigu, max_speed);
 using namespace cv;
 using namespace std;
 
-bool stopbool = false;
+bool refstart = false;
+bool stopbool = true;
 bool bl = false;
+String my_robotID = "A";
+String my_field = "A";
+//String goal = "yellow";
+String goal = "blue";
+bool respond = false;
 
 class SerialClass{
 public:
@@ -40,8 +46,10 @@ public:
 	{
 		//Stop the I/O services
 		io.stop();
+		
 		//Wait for the thread to finish
-		runner.join();
+		//runner.join();
+		runner.detach();
 	}
 
 	bool connect(const std::string& port_name, int baud = 19200)
@@ -65,7 +73,7 @@ public:
 
 			startReceive();
 		}
-		else{
+		else if ((port.is_open()) && (port_name.compare("COM3"))){
 			runner = boost::thread(
 				boost::bind(
 				&boost::asio::io_service::run,
@@ -90,20 +98,24 @@ public:
 			this, _1, _2));
 	}
 
+
 	void listenReferee()
 	{
 		using namespace boost::asio;
 		//Issue a async receive and give it a callback
 		//onData that should be called when "\r\n"
 		//is matched.
-		async_read_until(port, buffer,
-			"\r",
-			boost::bind(&SerialClass::onData,
-			this, _1, _2));
+		//async_read_until(port, buffer,"-",boost::bind(&SerialClass::onData,this, _1, _2));
+		async_read_until(port, buffer, "\r", boost::bind(&SerialClass::onData, this, _1, _2));
 	}
 
 
 	void send(const std::string& text)
+	{
+		boost::asio::write(port, boost::asio::buffer(text));
+	}
+
+	void send2(const std::string& text)
 	{
 		boost::asio::write(port, boost::asio::buffer(text));
 	}
@@ -115,13 +127,15 @@ public:
 		if (!e)
 		{
 			std::istream is(&buffer);
+			//std::istream is2(&buffer);
 			std::string data(size, '\0');
 			is.read(&data[0], size);
-
+			
 			if (((data.length()>6) && (data.compare("<4:bl:0>\n")==0)) || ((data.length()>6)&&(data.compare("<4:bl:1>\n")==0))){
 				
 				char bl_det = data[6];
-				cout << data<< endl;
+				//cout << data<< endl;
+				//cout << bl_det;
 				if (bl_det == '0'){
 					bl = false;
 				}
@@ -130,25 +144,30 @@ public:
 				}
 			}
 			else{
-			//cout << data << endl;
-			char my_robotID = 'A';
-			char my_field = 'A';
+			cout << data << endl;
+			//cout <<"0 " <<  data[0] << " " << data[1] << " " << data[2] << endl;
 			if ((data[0] == 'a')){//kohtinuku käsuks piisavalt pikk ja algab 'a'-ga
-				if (data[1] == my_field){//command is for my field
-					if ((data[2] == 'X') || (data[2] == my_robotID)){//command is for everybody
-						if (data[5] == 'A'){//command is START
+				if ((data[1] == 'A') || (data[1] == 'X')){//command is for my field
+					if ((data[2] == 'X') || (data[2] == 'A')){//command is for everybody
+						if (data[2] == my_robotID[0]){
+							respond = true;
+						}
+						if (data[5] == 'O'){//command is START
+							cout << "stopping..." << endl;
+							refstart = false;
+							stopbool = true;
+						}
+						else{//command is STOP
+						
+							refstart = true;
 							cout << "starting...." << endl;
 							stopbool = false;
-						}
-						else if (data[5] == 'O'){//command is STOP
-							cout << "stopping..." << endl;
-							stopbool = true;
 						}
 					}
 				}
 			}
 			}
-
+			cout << data << endl;
 			//If we receive quit()\r\n indicate
 			//end of operations
 			quitFlag = (data.compare("quit()\r\n") == 0);
@@ -161,17 +180,19 @@ public:
 
 private:
 	boost::asio::io_service io;
+	//boost::asio::io_service io2;
 	boost::asio::serial_port port;
 
 	boost::thread runner;
+	//boost::thread runner2;
 	boost::asio::streambuf buffer;
-
+	//boost::asio::streambuf buffer2;
 	bool quitFlag;
 };
 
 //global stuff
-vector< vector<Point> > contours_ball, contours_goal1, contours_goal2, contours_black;
-vector< Vec4i > hierarchy_ball, hierarchy_goal, hierarchy_black;
+vector< vector<Point> > contours_ball, contours_goal1, contours_goal2, contours_black, contours_white;
+vector< Vec4i > hierarchy_ball, hierarchy_goal, hierarchy_black, hierarchy_white;
 
 bool dribbler;
 bool suund;
@@ -227,7 +248,7 @@ void kick(SerialClass& serial);
 void move_robot(int * kiirus, SerialClass& serial);
 void ball_in(Point2f mc_goal, SerialClass& serial);
 void no_ball(Point2f mc_ball, SerialClass& serial);
-
+float eucl_dist(Point2f corner1, Point2f corner2);
 
 void sleepcp(int milliseconds) // cross-platform sleep function
 {
@@ -250,24 +271,28 @@ Mat preprocess(Mat frame, int lowH, int lowS, int lowV, int highH, int highS, in
 }
 
 //get mass center of goal(mc of biggest contour)
-Point2f process_goal(vector<vector<Point>> contours, Mat frame, Scalar varv) {
+tuple<Point2f, Point2f, Point2f> process_goal(vector<vector<Point>> contours, Mat frame, Scalar varv) {
 	float biggest_contour_area = 0;
 	int biggest_contour_id = -1;
 	vector<Point2f> mc_goal(contours.size());
 	for (int i = 0; i < contours.size(); i++) {
 		//drawContours(imgDrawing2, contours, i, Scalar(255, 0, 0), 1, 8, hierarchy_goal, 0, Point());
 		float ctArea = contourArea(contours[i]);
-		if (ctArea > biggest_contour_area) {
+		if ((ctArea > biggest_contour_area)&&(ctArea>500)) {
 			biggest_contour_area = ctArea;
 			biggest_contour_id = i;
 		}
 	}
 
-	if (biggest_contour_id < 0) {
-		Point2f temp;
+	if ((biggest_contour_id < 0)) {
+		Point2f temp, temp2, temp3;
 		temp.x = -1;
 		temp.y = -1;
-		return temp;//väravat ei leitud
+		temp2.x = -1;
+		temp2.y = -1;
+		temp3.x = -1;
+		temp3.y = -1;
+		return make_tuple(temp, temp2,temp3);;//väravat ei leitud
 	}
 	else {
 		vector<Moments> mu_goal(contours.size());
@@ -281,22 +306,66 @@ Point2f process_goal(vector<vector<Point>> contours, Mat frame, Scalar varv) {
 
 		RotatedRect boundingBox = minAreaRect(contours[biggest_contour_id]);
 		Point2f corners[4];
+		corners[0].x = -1;
+		corners[0].y = 1;
+		corners[1].x = -1;
+		corners[1].y = 1;
+		corners[2].x = -1;
+		corners[2].y = 1;
+		corners[3].x = -1;
+		corners[3].y = 1;
 		boundingBox.points(corners);
-		line(frame, corners[0], corners[1], varv);
-		line(frame, corners[1], corners[2], varv);
-		line(frame, corners[2], corners[3], varv);
-		line(frame, corners[3], corners[0], varv);
 
-		return mc_goal[biggest_contour_id];
+		float longest = -1;
+		int longest_id = -1;
+		if (eucl_dist(corners[0], corners[1]) > longest){
+			longest_id = 0;
+			longest = eucl_dist(corners[0], corners[1]);
+		}
+		if (eucl_dist(corners[1], corners[2]) > longest){
+			longest_id = 1;
+			longest = eucl_dist(corners[1], corners[2]);
+		}
+		if (eucl_dist(corners[2], corners[3]) > longest){
+			longest_id = 2;
+			longest = eucl_dist(corners[2], corners[3]);
+		}
+		if (eucl_dist(corners[3], corners[0]) > longest){
+			longest_id = 3;
+			longest = eucl_dist(corners[3], corners[0]);
+		}
+		line(frame, corners[0], corners[1], Scalar(255, 0, 0));
+		line(frame, corners[1], corners[2], Scalar(255, 0, 0));
+		line(frame, corners[2], corners[3], Scalar(255, 0, 0));
+		line(frame, corners[3], corners[0], Scalar(255, 0, 0));
+
+		if (longest_id == 0){
+			line(frame, corners[0], corners[1], Scalar(0, 0, 255));
+			return make_tuple(mc_goal[biggest_contour_id], corners[0], corners[1]);
+		}
+		else if (longest_id == 1){
+			line(frame, corners[1], corners[2], Scalar(0, 0, 255));
+			return make_tuple(mc_goal[biggest_contour_id], corners[1], corners[2]);
+		}
+		else if (longest_id == 2){
+			line(frame, corners[2], corners[3], Scalar(0, 0, 255));
+			return make_tuple(mc_goal[biggest_contour_id], corners[2], corners[3]);
+		}
+		else if (longest_id == 3){
+			line(frame, corners[3], corners[0], Scalar(0, 0, 255));
+			return make_tuple(mc_goal[biggest_contour_id], corners[3], corners[0]);
+		}
+
+		//return mc_goal[biggest_contour_id];
 	}
 }
 
+float eucl_dist(Point2f corner1, Point2f corner2){
+	return (sqrt((corner1.x - corner2.x)*(corner1.x - corner2.x) + (corner1.y - corner2.y)*(corner1.y - corner2.y)));
+}
+
 //väljastab ainult kõige suurema kontuuri, raadiuse
-pair<Point2f, float> process_ball(vector<vector<Point>> contours, Mat frame, Point2f mc_black) {
-	float b_x = mc_black.x;
-	float b_y = mc_black.y;
-	
-	float black_dist = sqrt((b_x - 320)*(b_x - 320) + (b_y - 480)*(b_y - 480));
+pair<Point2f, float> process_ball(vector<vector<Point>> contours, Mat frame, Point2f a, Point2f b) {
 
 	vector<Moments> mu(contours.size());
 	for (int i = 0; i < contours.size(); i++)
@@ -326,21 +395,62 @@ pair<Point2f, float> process_ball(vector<vector<Point>> contours, Mat frame, Poi
 		for (int i = 0; i < contours.size(); i++) {
 			//drawContours(imgDrawing2, contours, i, Scalar(255, 0, 0), 1, 8, hierarchy_goal, 0, Point());
 			float ctArea = contourArea(contours[i]);
-			if (ctArea > biggest_contour_area) {
+			if (ctArea > biggest_contour_area) { //if below line
+				circle(frame, Point(320, 480), 4, Scalar(0, 0, 0), 1, 8, 0);
+				/*
+				Mat temp, temp2, temp3 = Mat::zeros(frame.size(), CV_8UC3);
+				line(temp, a, b, Scalar(255, 255, 255), 4, 8, 0);
+				line(temp2, Point(320, 480), mc[i], Scalar(255, 255, 255), 4, 8, 0);
+				bitwise_and(temp, temp2, temp3);
+				double min, max;
+				int countw = 0;
+				for (int y = 0; y < temp3.rows; y++){
+				for (int x = 0; x < temp3.cols; x++){
+				if (temp3.at<cv::Vec3b>(y, x) == cv::Vec3b(0, 0, 0)){
+				countw += 1;
+				}
+
+				}
+				}
+				if (countw > 0){
+				cout << "found ball" << endl;
 				float ball_dist = sqrt((mc[i].x - 320)*(mc[i].x - 320) + (mc[i].y - 480)*(mc[i].y - 480));
-				//if (sqrt(ctArea / 3.1415)>1500){//take black lines into account only if ball is relatively close
-				if (ball_dist < black_dist){
+
+				biggest_contour_area = ctArea;
+				biggest_contour_id = i;
+				}
+				}
+				*/
+				//if (a.x != -1){
+				if (((((mc[i].x - a.x)*(b.y - a.y) - (mc[i].y - a.y)*(b.x - a.x))*((320 - a.x)*(b.y - a.y) - (480 - a.y)*(b.x - a.x))) < 0)
+					&& ((((a.x - mc[i].x)*(480 - mc[i].y) - (a.y - mc[i].y)*(320 - mc[i].x))*((b.x - mc[i].x)*(480 - mc[i].y) - (b.y - mc[i].y)*(320 - mc[i].x))) < 0)){
+					//cout << "found ball" << endl;
+					float ball_dist = sqrt((mc[i].x - 320)*(mc[i].x - 320) + (mc[i].y - 480)*(mc[i].y - 480));
+
 					biggest_contour_area = ctArea;
 					biggest_contour_id = i;
 				}
 				
-				/*}
 				else{
+					float ball_dist = sqrt((mc[i].x - 320)*(mc[i].x - 320) + (mc[i].y - 480)*(mc[i].y - 480));
+
+					biggest_contour_area = ctArea;
+					biggest_contour_id = i;
+				//}
+				}
+				/*
+				else if (((((mc[i].x - a.x)*(b.y - a.y) - (mc[i].y - a.y)*(b.x - a.x))*((320 - a.x)*(b.y - a.y) - (480 - a.y)*(b.x - a.x))) > 0)
+					&& ((((a.x - mc[i].x)*(480 - mc[i].y) - (a.y - mc[i].y)*(320 - mc[i].x))*((b.x - mc[i].x)*(480 - mc[i].y) - (b.y - mc[i].y)*(320 - mc[i].x))) > 0)){
+					cout << "found ball" << endl;
+					float ball_dist = sqrt((mc[i].x - 320)*(mc[i].x - 320) + (mc[i].y - 480)*(mc[i].y - 480));
+
 					biggest_contour_area = ctArea;
 					biggest_contour_id = i;
 				}*/
 			}
-		}
+			
+			}
+		
 		float r = sqrt(biggest_contour_area / 3.1415);
 		//cout << "r= " << biggest_contour_area;
 		if (biggest_contour_id != -1){
@@ -453,9 +563,9 @@ void set_dribbler(int speed, SerialClass& serial){
 }
 
 void ball_in(Point2f mc_goal, SerialClass& serial){//ball in dribbler
-	int hs = 100;
-	int ms = 50;
-	int ls = 0;
+	int hs = 150;
+	int ms = 75;
+	int ls = 50;
 
 	if (mc_goal.x == -1){
 		//search for ball
@@ -474,35 +584,38 @@ void ball_in(Point2f mc_goal, SerialClass& serial){//ball in dribbler
 		float liigu[3] = { 0, 0, 0 };//par4emale
 		movement(liigu, 0, serial);
 		serial.send("c\r\n");
-		sleepcp(2000);
+		sleepcp(1000);
+		//set_dribbler(0, serial);
 		serial.send("k\r\n");
+		serial.send("c\r\n");
+		//set_dribbler(255, serial);
 		bl = false;
 	}
 }
 
 void no_ball(Point2f mc_ball, float kaugus, SerialClass& serial){
-	int hs = 120;
-	int ms = 50;
-	int ls = 30;
-
+	int hs = 75;
+	int ms = 75;
+	int ls = 50;
+	/*
 	hs = 0;
 	ms = 0;
 	ls = 0;
-
+	*/
 	if (mc_ball.x == -1){
 		//search for ball
 		float liigu[3] = { 0, -0.3, -0.5 };//paremale
 		movement(liigu, ls, serial);
 	}
-	else if (mc_ball.x < 275){
+	else if (mc_ball.x < 255){
 		float liigu[3] = { 0, 0.3, 0.5 };//vasakule
 		movement(liigu, ms, serial);
 	}
-	else if (mc_ball.x > 365){
+	else if (mc_ball.x > 385){
 		float liigu[3] = { 0, -0.3, -0.5 };//par4emale
 		movement(liigu, ms, serial);
 	}
-	else if ((mc_ball.x > 274) && (mc_ball.x < 366)){
+	else if ((mc_ball.x > 254) && (mc_ball.x < 386)){
 		float liigu[3] = { 1, 0, 0 };//otse
 		if (kaugus > 150){
 			movement(liigu, hs, serial);
@@ -517,7 +630,7 @@ void no_ball(Point2f mc_ball, float kaugus, SerialClass& serial){
 }
 
 tuple<Mat, Point2f, Point2f, float> get_frame(VideoCapture cap, String goal){
-	Mat frame, pall_thresh, goal_thresh, black_thresh, black_result;
+	Mat frame, pall_thresh, goal_thresh, black_thresh, black_result, white_thresh, white_result;
 	cap >> frame;
 	if (!cap.read(frame)) cout << "error reading frame" << endl;//check for error'
 
@@ -528,11 +641,21 @@ tuple<Mat, Point2f, Point2f, float> get_frame(VideoCapture cap, String goal){
 
 	findContours(pall_thresh, contours_ball, hierarchy_ball, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	black_thresh = preprocess(frame, 0, 0, 0, 180, 255, 30);
-	findContours(black_thresh, contours_black, hierarchy_black, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-	Point2f mc_black = process_goal(contours_black, frame, Scalar(0, 0, 0));
+	black_thresh = preprocess(frame, 0, 0, 0, 180, 255, 160);
+	white_thresh = preprocess(frame, 0, 0, 240, 180, 255, 255);
+	
+	dilate(white_thresh, white_thresh, Mat(), Point(-1, -1), 10);
+	bitwise_and(white_thresh, black_thresh, white_result);
 
-	pair<Point2f, float> result = process_ball(contours_ball, frame, mc_black);
+	imshow("test2", white_result);
+	waitKey(10);
+
+	findContours(white_result, contours_white, hierarchy_white, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+	Point2f mc_black, mc_white, corner1, corner2;
+	tie(mc_black, corner1, corner2) = process_goal(contours_black, frame, Scalar(0, 0, 0));
+	tie(mc_white, corner1, corner2)= process_goal(contours_white, frame, Scalar(255, 0, 0));
+	
+	pair<Point2f, float> result = process_ball(contours_ball, frame, corner1, corner2);
 	mc_ball = result.first;
 	float raadius = result.second;
 
@@ -557,27 +680,10 @@ tuple<Mat, Point2f, Point2f, float> get_frame(VideoCapture cap, String goal){
 		goal_thresh = preprocess(frame, G_lowH2, G_lowS2, G_lowV2, G_highH2, G_highS2, G_highV2);
 	}
 	
-	
-	/*
-	vector<Vec4i> lines;
-	HoughLinesP(black_thresh, lines, 1, CV_PI / 180, 200, 100, 10);
-	
-	
-	for (size_t i = 0; i < lines.size(); i++)
-	{
-		line(frame, Point(lines[i][0], lines[i][1]),
-			Point(lines[i][2], lines[i][3]), Scalar(255, 255, 255), 3, 8);
-	}
-	
-	*/
-	//imshow("goal", goal_thresh);
-	//waitKey(30);
-	//imshow("goal", goal_thresh);
-	//waitKey(30);
 	findContours(goal_thresh, contours_goal1, hierarchy_goal, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-	Point2f mc_goal = process_goal(contours_goal1, frame, Scalar(255, 255, 255));
-
+	
+	Point2f mc_goal, lamp0, lamp1;
+	tie(mc_goal, lamp0, lamp1 ) = process_goal(contours_goal1, frame, Scalar(255, 255, 255));
 	return make_tuple(frame, mc_ball, mc_goal, palli_kaugus);
 }
 
@@ -609,65 +715,95 @@ int main() {
 
 	//connect to serial ports
 	SerialClass serial;//control motors etc
-
+	
 	if (serial.connect("COM3", 19200))
 	{
-		std::cout << "Port is open." << std::endl;
+		std::cout << "Port COM3 is open." << std::endl;
 	}
 	else
 	{
 		std::cout << "Port open failed." << std::endl;
 	}
+	
 	SerialClass serialref;//referee
 
 	if (serialref.connect("COM4", 19200))
 	{
-		std::cout << "Port is open." << std::endl;
+		std::cout << "Port COM4 is open." << std::endl;
 	}
 	else
 	{
 		std::cout << "Port open failed." << std::endl;
 	}
-
+	
 	VideoCapture cap(0);//enter cam # or video location-------------------
 	if (!cap.isOpened()) return -1; //check if succeeded
 
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-	String goal = "yellow";
+	serialref.send("+++");
 
 	//serial.send("c\r\n");
 	//serial.send("dm255\r\n");
-
-	serialref.send("+++");
+	
+	serial.send("c\r\n");
+	//cout << "Referee test..." << endl;
+	//serialref.send("+++");
+	cout << "My robot ID: " << my_robotID[0] << endl;
+	cout << "My field ID: " << my_field[0] << endl;
+	cout << "My goal: " << goal << endl;
+	//stopbool = false;
 	bool tribler = false;
 	for (;;) {
+		
 		if (stopbool == true){
-			stop(stopbool, serial);
+			//cout << "stop" << endl;
+			//stop(stopbool, serial);
+			/*
+			if (serialref.connect("COM4", 19200))
+			{
+				std::cout << "Port COM4 is open." << std::endl;
+				serialref.send("+++");
+			}
+			else
+			{
+				std::cout << "Port open failed." << std::endl;
+			}
+			*/
+			
 		}
-
-		tie(frame, mc_ball, mc_goal, kaugus) = get_frame(cap, goal);
-		if ((kaugus < 100) && (tribler == false) && (kaugus >0)){
-			set_dribbler(255, serial);
-		}
-		if (((kaugus > 100) && (tribler == true))||(kaugus==-1)&&(bl==false)){
+		else{
+			//cout << "start" << endl;
 			set_dribbler(0, serial);
-		}
-		serial.send("bl\r\n");
-		sleepcp(10);
-		if (bl == true){
-			ball_in(mc_goal, serial);
-			//set_dribbler(200, serial);
-			if (tribler == false){
-				set_dribbler(255, serial);
-				tribler = true;
+			if (respond == true){
+				String temp = "a" + my_field + my_robotID + "ACK-----\r";
+				//serialref.send(temp);
+				respond = false;
 			}
-		}
-		else {
-			no_ball(mc_ball, kaugus, serial);
+
+			tie(frame, mc_ball, mc_goal, kaugus) = get_frame(cap, goal);
+			if ((kaugus < 100) && (tribler == false) && (kaugus >0)){
+				//set_dribbler(255, serial);
 			}
-		imshow("orig", frame);
-		waitKey(10);
+			if (((kaugus > 100) && (tribler == true)) || (kaugus == -1) && (bl == false)){
+				//set_dribbler(0, serial);
+			}
+			serial.send("bl\r\n");
+			sleepcp(10);
+			if (bl == true){
+				ball_in(mc_goal, serial);
+				//set_dribbler(200, serial);
+				if (tribler == false){
+					
+					tribler = true;
+				}
+			}
+			else {
+				no_ball(mc_ball, kaugus, serial);
+			}
+			imshow("orig", frame);
+			waitKey(10);
+		}
 	}
 
 
